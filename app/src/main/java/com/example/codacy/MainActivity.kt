d1cdeeb412d3869b78ebf50853027cb07137cf84
Hendra P Utama <hendra.utama@itmi.id>
2020-01-28 12:46:24 +0700

    try catch

--- app/src/main/java/com/example/codacy/MainActivity.kt
+++ app/src/main/java/com/example/codacy/MainActivity.kt
@@ -2,13 +2,19 @@ package com.example.codacy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        text.text = "Testing"
+        try {
+            text.text = "Testing"
+        } catch (e: Exception) {
+            Log.e("ERROR", e.message)
+        }
     }
 }
